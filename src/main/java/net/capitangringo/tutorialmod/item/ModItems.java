7cadc5183f2307db453d33c4757eae184f2190f4
Tyler M. <58830692+TylerJMorgan@users.noreply.github.com>
2025-08-08 17:00:25 -0700

    Initial commit

---
+++ src/main/java/net/capitangringo/tutorialmod/item/ModItems.java
@@ -0,0 +1,25 @@
+package net.capitangringo.tutorialmod.item;
+
+import net.capitangringo.tutorialmod.TutorialMod;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+// Some Notes: It is not necessary to create a ModItems class (1/4),
+// The en_us.json gives the registered item a proper name (2/4)
+// The models/item/bismuth.json makes sure to point to the correct texture (3/4)
+// Make sure the textures (assets/tutorialmod/textures) are 16x16 pngs (if you want it to match other Minecraft stuff)
+public class ModItems {
+    // A deferred register helps to register objects to modded and vanilla registries, when the time comes it will
+    // tell Minecraft to also add these items under our Mod ID.
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(TutorialMod.MOD_ID);
+
+    // This line takes care of the registration of the item/it registers a new item (4/4)
+    public static final DeferredItem<Item> BISMUTH = ITEMS.register("bismuth", () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> RAW_BISMUTH = ITEMS.register("raw_bismuth", () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
