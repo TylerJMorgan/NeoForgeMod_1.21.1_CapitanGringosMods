60fccaaeecdb473e27eddfe7e95fd010a24a3191
Tyler <tjmorga7@asu.edu>
2025-08-27 16:01:15 -0700

    [Slingshot] Remove Bismuth, core mod finished

--- src/main/java/net/gringo/capitangringosmods/CapitanGringosMods.java
+++ src/main/java/net/gringo/capitangringosmods/CapitanGringosMods.java
@@ -57,8 +57,6 @@ public class CapitanGringosMods {
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
         if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
-            event.accept(ModItems.BISMUTH);
-            event.accept(ModItems.RAW_BISMUTH);
             event.accept(ModItems.PEBBLE);
             event.accept(ModItems.SLINGSHOT);
             event.accept(ModItems.POISON_PEBBLE);
--- src/main/java/net/gringo/capitangringosmods/item/ModItems.java
+++ src/main/java/net/gringo/capitangringosmods/item/ModItems.java
@@ -11,7 +11,7 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 
 // Some Notes: It is not necessary to create a ModItems class (1/4),
 // The en_us.json gives the registered item a proper name (2/4)
-// The models/item/bismuth.json makes sure to point to the correct texture (3/4)
+// The models/item/.json makes sure to point to the correct texture (3/4)
 // Make sure the textures (assets/capitangringosmods/textures) are 16x16 pngs (if you want it to match other Minecraft stuff)
 public class ModItems {
     // A deferred register helps to register objects to modded and vanilla registries, when the time comes it will
@@ -19,8 +19,6 @@ public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(CapitanGringosMods.MOD_ID);
 
     // This line takes care of the registration of the item/it registers a new item (4/4)
-    public static final DeferredItem<Item> BISMUTH = ITEMS.register("bismuth", () -> new Item(new Item.Properties()));
-    public static final DeferredItem<Item> RAW_BISMUTH = ITEMS.register("raw_bismuth", () -> new Item(new Item.Properties()));
     public static final DeferredItem<Item> PEBBLE = ITEMS.register("pebble", () -> new Item(new Item.Properties()));
     public static final DeferredItem<Item> SLINGSHOT = ITEMS.register("slingshot",
             () -> new SlingshotItem(new Item.Properties().durability(150)));
